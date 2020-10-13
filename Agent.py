ef291e8d3c2ff5c21e7694e533bc0c74110aef0a
Alena <alena195101@yandex.ru>
2020-10-14 01:10:30 +0300

    дополнила реализацию с классом агента

--- Agent.py
+++ Agent.py
@@ -1,8 +1,13 @@
+import numpy as np
+import pandas as pd
+
 class Give_me_money:
-    def __init__(self):
+    def __init__(self, alpha):
         self.matrix = pd.DataFrame({'ind':np.arange(10), 'Q':[0.5] * 10})
         self.move = None
         self.N = 0
+        self.c = alpha
+        self.alpha = alpha
     def get_action(self):
         self.N += 1
         if np.random.random() < 0.1:
@@ -13,4 +18,6 @@ class Give_me_money:
             return self.move
     def is_done(self, reward):
         old_value = self.matrix.loc[self.move, 'Q']
-        self.matrix.loc[self.move, 'Q'] = old_value + (1/self.N)*(reward - old_value)
+        if self.c is None:
+            self.alpha = (1/self.N)
+        self.matrix.loc[self.move, 'Q'] = old_value + self.alpha*(reward - old_value)
